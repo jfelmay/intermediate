82d2177f99c76e1cb1a32116edf0a9528c09139d
JFEL <felix@example.com>
2019-06-25 20:05:27 +0000

    fin del martes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+packege 'httpd'
+
+file '/var/www/html/index.html' do
+	context '<h1>Welcome Home!</h1>'
+end
+
+Service 'httpd' do
+	action [:enable, :start]
+end	
